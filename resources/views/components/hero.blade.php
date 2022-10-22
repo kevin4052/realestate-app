1a229790f30a3f76cea2f6d156b5d2cc3639bbe4
Kevin Hernandez <kevin.hernandez001@gmail.com>
2022-10-22 19:39:13 -0400

    hero section

---
+++ resources/views/components/hero.blade.php
@@ -0,0 +1,51 @@
+
+
+<div class="hero">
+    <div class="hero__images">
+        <div class="hero__image">
+            <img
+                src="https://images.dwell.com/photos-6311371353569939456/6739650785763569664-large/modern-architectural-style-with-warm-finishes-to-compliment-mountain-setting.jpg"
+                class="hero__image-img"/>
+            <div class="hero__info">
+                <span class="hero__subtitle"> Residential </span>
+                <span class="hero__title"> 123 Road </span>
+                <a href="#" class="hero__info-link"> See Property </a>
+            </div>
+        </div>
+        <div class="hero__image">
+            <img
+            src="https://www.architectureartdesigns.com/wp-content/uploads/2019/01/modern-box-home-design.jpg"
+            class="hero__image-img"/>
+            <div class="hero__info">
+                <span class="hero__subtitle"> Residential </span>
+                <span class="hero__title"> 123 Road </span>
+                <a href="#" class="hero__info-link"> See Property </a>
+            </div>
+        </div>
+        <div class="hero__image">
+            <img
+            src="https://www.gannett-cdn.com/presto/2021/03/02/PNDN/f54f2159-946d-4592-9d83-c174ec40199c-GSH_Modern_Arch_0307.jpg?crop=1132,637,x0,y22&width=1132&height=637&format=pjpg&auto=webp"
+            class="hero__image-img"/>
+            <div class="hero__info">
+                <span class="hero__subtitle"> Residential </span>
+                <span class="hero__title"> 123 Road </span>
+                <a href="#" class="hero__info-link"> See Property </a>
+            </div>
+        </div>
+    </div>
+    <div class="hero__indicator">
+        <div class="hero__indicator-item">
+            <span class="hero__indicator-number">01</span>
+            <span class="hero__indicator-text">Summer Villa</span>
+        </div>
+        <div class="hero__indicator-item">
+            <span class="hero__indicator-number">02</span>
+            <span class="hero__indicator-text">Summer Villa</span>
+        </div>
+        <div class="hero__indicator-item">
+            <span class="hero__indicator-number">03</span>
+            <span class="hero__indicator-text">Summer Villa</span>
+        </div>
+    </div>
+</div>
+
