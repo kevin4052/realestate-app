d014543f5a268db608981c32b7cc74c490b77db6
Kevin Hernandez <kevin.hernandez001@gmail.com>
2022-10-24 21:27:05 -0400

    listings page items

--- resources/views/pages/listings.blade.php
+++ resources/views/pages/listings.blade.php
@@ -17,6 +17,31 @@
             <div class="listings-filter__button">Search</div>
         </div>
     </div>
+
+    <div class="listings-properties">
+        <div class="container">
+            <div class="row">
+                @for ($i = 0; $i < 10; $i++)
+                <div class="col-md-6 col-lg-4 col-xl-3">
+                    <div class="listings-properties__item">
+                        <img class="" src="https://www.architectureartdesigns.com/wp-content/uploads/2019/01/modern-box-home-design.jpg" alt="listing image">
+                        <div class="listings-properties__item-info">
+                            <span class="listings-properties__item-price">$250,000</span>
+                            <span class="listings-properties__item-details">
+                                <i class="fa-solid fa-bed"></i> 4 
+                                <i class="fa-solid fa-bath"></i> 4 
+                                <i class="fa-solid fa-ruler"></i> 4,000 SQFT
+                            </span>
+                            <span class="listings-properties__item-address">123 Street, <br/> Miami, FL 12345</span>
+                            <div class="listings-properties__item-line"></div>
+                            <span class="listings-properties__item-owner">Kevin Hernandez</span>
+                        </div>
+                    </div>        
+                </div>
+                @endfor
+            </div>
+        </div>
+    </div>
 </div>
 
 @endsection
