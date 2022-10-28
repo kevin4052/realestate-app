a2fbfec46cdf8c086b4b03c9a2db5e8d15d00dad
Kevin Hernandez <kevin.hernandez001@gmail.com>
2022-10-27 21:28:06 -0400

    show listing status page

--- resources/views/pages/show-status.blade.php
+++ resources/views/pages/show-status.blade.php
@@ -6,6 +6,27 @@
 
 @section('content')
 <div class="request-list">
-
+    <h2>All Requests</h2>
+    @for ($i = 0; $i < 5; $i++)
+    <div class="request-list__wrapper">
+        <div>
+            <h3>1234 Grand Ave, Miami Beach, FL 33333 </h3>
+            <span class="request-list__details">
+                <i class="fa-solid fa-bed"></i> 4 
+                <i class="fa-solid fa-bath"></i> 4 
+                <i class="fa-solid fa-ruler"></i> 4,000 SQFT
+            </span>
+        </div>
+        <div class="request-list__info">
+            <span class="request-list__info-title">status</span>
+            <span class="request-list__info-status 
+                request-list__info-status--success 
+                request-list__info-status--canceled 
+                request-list__info-status--sold">
+                Success
+            </span>
+        </div>
+    </div>
+    @endfor
 </div>
 @endsection
